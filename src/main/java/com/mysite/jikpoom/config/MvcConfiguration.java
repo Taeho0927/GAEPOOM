d884540833ed1a8445be2f41afe8505b9a65b8a9
Taeho0927 <rlfxogh0927@gmail.com>
2022-11-26 18:03:30 +0900

    update, index페이지 구성

--- src/main/java/com/mysite/jikpoom/config/MvcConfiguration.java
+++ src/main/java/com/mysite/jikpoom/config/MvcConfiguration.java
@@ -13,6 +13,6 @@ public class MvcConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(60*60*24*365);
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(60*60*24*365);
         registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/").setCachePeriod(60*60*24*365);
-        //registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/").setCachePeriod(60*60*24*365);
+        registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/").setCachePeriod(60*60*24*365);
     }
 }
